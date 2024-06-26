e2be1f7eea3e4c9ecde733d6c2a0eac59d976b02
Harsh <harsh.2024it1034@kiet.edu>
2024-06-26 22:53:04 +0530

    Done some changes in the file

--- learning/sum.cpp
+++ learning/sum.cpp
@@ -3,7 +3,10 @@ using namespace std;
 
 int sum(int a, int b) {return a + b;}
 
+int diff(int a, int b) {return abs(a-b);}
+
 int main()
 {
     cout << sum(2, 3) << endl;
+    cout << diff(2, 3) << endl;
 }
